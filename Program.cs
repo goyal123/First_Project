d03be450bd19fa3f7710483400d7802ca3695d51
goyal123 <akhilg642@gmail.com>
2022-07-11 14:49:37 +0530

    Calling code of Reverse_Number in Program file

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
+            int num,reverse_number;
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a Number");
+            num = Convert.ToInt32(Console.ReadLine());
+            reverse_number=Reverse_Number.Reverse(num);
+            Console.WriteLine("Reversed Number = {0} ",reverse_number);
+
+
         }
     }
 }
