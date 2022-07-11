5d7bbed67ec6e30c47d07c57c8c7c61158b357d5
goyal123 <akhilg642@gmail.com>
2022-07-11 14:47:10 +0530

    Code of Reverse_Number

---
+++ Reverse_Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programs
+{
+    public class Reverse_Number
+    {
+        public static int Reverse(int number)
+        {
+            int remainder = 0, reverse = 0;
+            while(number!=0)
+            {
+                remainder=number % 10;
+                reverse = remainder + (reverse * 10);
+                number=number/10;
+            }
+            return reverse;
+        }
+        
+
+    }
+}
